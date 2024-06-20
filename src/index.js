fc9101fd1ecadada285294023225b01ad4a547cc
Richard Aurélien <aurelien.r@hotmail.ch>
2024-06-20 19:22:06 +0200

    adding 1 word to array everytime

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import { Player } from "textalive-app-api";
 
 // 単語が発声されていたら #text に表示する
 
+
+class WordProjectile {
+    constructor(text, x, y) {
+        this.text = text;
+        this.x = x;
+        this.y = y;
+    }
+}
+
 const animateWord = function (now, unit) {
 
     if (unit.contains(now)) {
-
-        document.querySelector("#text").textContent = unit.text;
+        //console.log(unit.text)
+        let word = new WordProjectile(unit.text, 10, 10);
+        document.querySelector("#text").textContent += unit.text;
+        //console.log(word.text);
 
     }
 
@@ -15,6 +26,9 @@ const animateWord = function (now, unit) {
 const player = new Player({ app: { token: "eZ2xkHnnUWrJKQRG" }, mediaElement: document.querySelector("#media") });
 
 const playBtn = document.querySelector("#play");
+const pauseBtn = document.querySelector("#pause");
+
+const words = [];
 
 player.addListener({
     onAppReady: (app) => {
@@ -25,6 +39,13 @@ player.addListener({
                 player.video &&
                 player.requestPlay()
         );
+        pauseBtn.addEventListener(
+            "click",
+            () =>
+                player.video &&
+                player.requestPause()
+        );
+
 
         if (!app.songUrl) {
             // フューチャーノーツ / shikisai
@@ -39,6 +60,8 @@ player.addListener({
                     lyricDiffId: 13964
                 },
             });
+            player.volume = 5;
+
         }
     },
 
@@ -46,18 +69,9 @@ player.addListener({
 
     onVideoReady: (v) => {
 
-        // 定期的に呼ばれる各単語の "animate" 関数をセットする
-
-        let w = player.video.firstWord;
 
 
-        while (w) {
 
-            w.animate = animateWord;
-
-            w = w.next;
-
-        }
 
     },
 
@@ -67,6 +81,32 @@ player.addListener({
         if (!player.app.managed) {
             playBtn.disabled = false;
         }
+    },
+
+    onTimeUpdate: (position) => {
+
+        const current = player.timer.position;
+        //console.log(position)
+
+        // 定期的に呼ばれる各単語の "animate" 関数をセットする
+        let w = player.video.firstChar;
+        let lyrics = "";
+        while (w && w.startTime < current) {
+            console.log("-----------------------------------");
+            if (!words.includes(w)) { // maybe store the last startime and compare it to this instead of a full array search ?
+
+                words.push(w);
+
+                // Do what we want with the new word
+                //w.animate = animateWord;
+                document.querySelector("#text").textContent += w.text;
+            }
+            //lyrics += w.text;
+            // w.animate = animateWord;
+            w = w.next;
+        }
     }
 
 });
+
+
