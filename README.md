004c31beb2c5d6a14da111be24baf6fc20c5c8cb
Richard Aurélien <aurelien.r@hotmail.ch>
2024-07-09 05:41:22 +0200

    last commit I guess

---
+++ README.md
@@ -0,0 +1,52 @@
+# Hatsune Miku Magical Mirai 2024 - Sekai Basket
+Welcome to my participation for the 2024 Hatsune Miku Magical Mirai contest !
+
+In here you will find a simplistic web game built with the [TextAlive API](https://developer.textalive.jp/) !
+
+I hope you will have some fun with the application !
+
+## The game
+The game is very simple !
+
+It is similar to the games where you have to collect fruits falling from the sky into a basket, but here you will collect words from the songs that are being played !
+
+To do so, just move your mouse from left to right and try to catch them before they reach the bottom of the page :)
+
+Catching them in a row will increase a mulitplier, but missing one will drop it right back down to 0 !
+
+
+Example : 
+![Example of the game running](Sekai_Basket_example.gif)
+(GIF fps way slower than the app ofc)
+
+## Run the application
+After cloning the repository you can run the following commands to get the application running.
+```sh
+npm install
+npm run dev
+```
+## Change songs
+If you wish to change the song that is being played you can change it in the code by simply commenting/uncommenting the chosen code : 
+
+(song with beatId 4592299 currently chosen in the screenshot)![image showing commented and uncommented song selection](change_songs.png)
+Tip : in Visual Studio code you can uncomment a block of code using *CTRL + K + U* and mass commenting with *CTRL + K + C*
+
+## Build
+If you wish to do so you can build the application with vite using this command (warning: it wasn't tested much due to lack of time... if possible to use npm run dev, please prefer it as I know it works)
+```sh
+npm run build
+```
+
+## Potential improvements
+As with all my projects I like to think about what could be improved.
+With this web app there is so much that could be done but here is a small list of improvements I think would make it even better. 
+- Choosing song directly in app with a menu
+- Keeping the highest score in session or stored on the server
+- making it multiplayer by introducing sessions and leaderboards
+- keep the full random mode (letter falling randomly)
+- introduce a more controlled mode, where all runs would be the same (more fair for multiplayer)
+
+## Closing thoughts
+I wish I could have spent more time on it but I really enjoyed discovering the api and learning more JS !
+
+I do hope you will have fun with it !
--- src/index.js
+++ src/index.js
@@ -88,82 +88,82 @@ player.addListener({
 
         if (!app.songUrl) {
             // SUPERHERO / めろくる
-            player.createFromSongUrl("https://piapro.jp/t/hZ35/20240130103028", {
-                video: {
-                    // 音楽地図訂正履歴
-                    beatId: 4592293,
-                    chordId: 2727635,
-                    repetitiveSegmentId: 2824326,
-                    // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FhZ35%2F20240130103028
-                    lyricId: 59415,
-                    lyricDiffId: 13962
-                },
-            });
+            // player.createFromSongUrl("https://piapro.jp/t/hZ35/20240130103028", {
+            //     video: {
+            //         // 音楽地図訂正履歴
+            //         beatId: 4592293,
+            //         chordId: 2727635,
+            //         repetitiveSegmentId: 2824326,
+            //         // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FhZ35%2F20240130103028
+            //         lyricId: 59415,
+            //         lyricDiffId: 13962
+            //     },
+            // });
 
             // いつか君と話したミライは / タケノコ少年
             // player.createFromSongUrl("https://piapro.jp/t/--OD/20240202150903", {
-            //   video: {
-            //     // 音楽地図訂正履歴
-            //      beatId: 4592296,
-            //      chordId: 2727636,
-            //      repetitiveSegmentId: 2824327,
-            //      // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2F--OD%2F20240202150903
-            //      lyricId: 59416,
-            //      lyricDiffId: 13963
-            //    },
-            //  });
+            //     video: {
+            //         // 音楽地図訂正履歴
+            //         beatId: 4592296,
+            //         chordId: 2727636,
+            //         repetitiveSegmentId: 2824327,
+            //         // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2F--OD%2F20240202150903
+            //         lyricId: 59416,
+            //         lyricDiffId: 13963
+            //     },
+            // });
 
             // フューチャーノーツ / shikisai
-            // player.createFromSongUrl("https://piapro.jp/t/XiaI/20240201203346", {
-            //   video: {
-            //     // 音楽地図訂正履歴
-            //      beatId: 4592297,
-            //      chordId: 2727637,
-            //      repetitiveSegmentId: 2824328,
-            //      // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FXiaI%2F20240201203346
-            //      lyricId: 59417,
-            //      lyricDiffId: 13964
-            //    },
-            //  });
+            player.createFromSongUrl("https://piapro.jp/t/XiaI/20240201203346", {
+                video: {
+                    // 音楽地図訂正履歴
+                    beatId: 4592297,
+                    chordId: 2727637,
+                    repetitiveSegmentId: 2824328,
+                    // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FXiaI%2F20240201203346
+                    lyricId: 59417,
+                    lyricDiffId: 13964
+                },
+            });
 
             // 未来交響曲 / ヤマギシコージ
             // player.createFromSongUrl("https://piapro.jp/t/Rejk/20240202164429", {
-            //   video: {
-            //     // 音楽地図訂正履歴
-            //      beatId: 4592298,
-            //      chordId: 2727638,
-            //      repetitiveSegmentId: 2824329,
-            //      // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FRejk%2F20240202164429
-            //      lyricId: 59418,
-            //      lyricDiffId: 13965
-            //    },
-            //  });
+            //     video: {
+            //         // 音楽地図訂正履歴
+            //         beatId: 4592298,
+            //         chordId: 2727638,
+            //         repetitiveSegmentId: 2824329,
+            //         // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FRejk%2F20240202164429
+            //         lyricId: 59418,
+            //         lyricDiffId: 13965
+            //     },
+            // });
 
             // リアリティ / 歩く人
             // player.createFromSongUrl("https://piapro.jp/t/ELIC/20240130010349", {
-            //   video: {
-            //     // 音楽地図訂正履歴
-            //      beatId: 4592299,
-            //      chordId: 2727639,
-            //      repetitiveSegmentId: 2824330,
-            //      // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FELIC%2F20240130010349
-            //      lyricId: 59419,
-            //      lyricDiffId: 13966
-            //    },
-            //  });
+            //     video: {
+            //         // 音楽地図訂正履歴
+            //         beatId: 4592299,
+            //         chordId: 2727639,
+            //         repetitiveSegmentId: 2824330,
+            //         // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FELIC%2F20240130010349
+            //         lyricId: 59419,
+            //         lyricDiffId: 13966
+            //     },
+            // });
 
             // The Marks / 2ouDNS
             // player.createFromSongUrl("https://piapro.jp/t/xEA7/20240202002556", {
-            //   video: {
-            //     // 音楽地図訂正履歴
-            //      beatId: 4592300,
-            //      chordId: 2727640,
-            //      repetitiveSegmentId: 2824331,
-            //      // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FxEA7%2F20240202002556
-            //      lyricId: 59420,
-            //      lyricDiffId: 13967
-            //    },
-            //  });
+            //     video: {
+            //         // 音楽地図訂正履歴
+            //         beatId: 4592300,
+            //         chordId: 2727640,
+            //         repetitiveSegmentId: 2824331,
+            //         // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FxEA7%2F20240202002556
+            //         lyricId: 59420,
+            //         lyricDiffId: 13967
+            //     },
+            // });
             player.volume = 5;
 
         }
